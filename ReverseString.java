37d93ec9e7c632d1ac0908977db65584762d250f
Sweety0813 <ramyakaragiri@gmail.com>
2026-01-08 00:02:39 -0600

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,6 +1,12 @@
 public class ReverseString {
     public static void main(String[] args) {
         String input = "HelloWorld";
+
+        if (input == null || input.isEmpty()) {
+            System.out.println("Invalid input");
+            return;
+        }
+
         String reversed = "";
 
         for (int i = input.length() - 1; i >= 0; i--) {
