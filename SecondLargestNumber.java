630d53c42825fe11b62fcd394756f4ee6ee3a1c4
Sweety0813 <ramyakaragiri@gmail.com>
2026-01-07 22:08:06 -0600

    Create SecondLargestNumber.java

---
+++ SecondLargestNumber.java
@@ -0,0 +1,18 @@
+public class SecondLargestNumber {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 5, 8, 25};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println("Second Largest Number: " + secondLargest);
+    }
+}
